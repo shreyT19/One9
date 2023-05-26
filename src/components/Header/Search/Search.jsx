49556ac06d21446ed647297d834fbda1040571d2
shreyT19 <shreyanshtripathy49@gmail.com>
2023-05-26 13:53:44 +0530

    search added

--- src/components/Header/Search/Search.jsx
+++ src/components/Header/Search/Search.jsx
@@ -1,6 +1,28 @@
+import { MdClose } from "react-icons/md";
 import "./Search.scss";
-const Search = () => {
-    return <div>Search</div>;
+import prod from '../../../assets/products/earbuds-prod-2.webp'
+const Search = ({setShowSearch}) => {
+  return (
+    <div className="search-modal">
+      <div className="form-field">
+        <input type="text" autoFocus placeholder="Search for Products" />
+        <MdClose className="close-btn" onClick={()=>setShowSearch(false)}/>
+      </div>
+      <div className="search-result-content">
+        <div className="search-results">
+            <div className="search-result-item">
+                <div className="img-container">
+                    <img src={prod} alt="" />
+                </div>
+                <div className="product-details">
+                    <span className="name">product name</span>
+                    <span className="description">product name</span>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,21 @@
 import "./Home.scss";
 import Banner from "./Banner/Banner";
-
+import { useEffect } from "react";
 import Category from "./Category/Category.jsx";
 import Products from "../Products/Products";
 // import SingleProduct from "../SingleProduct/SingleProduct";
+
+import { fetchDataFromAPI } from "../../utils/api";
 const Home = () => {
+  useEffect(()=>{
+    getCategories()
+  },[])
+
+  const getCategories = ()=>{
+    fetchDataFromAPI("/api/categories").then(res=>console.log(res))
+  }
+
+
   return (
     <div>
       <Banner />
